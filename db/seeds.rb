e79abd1dc3da4fa9bac8f2f8647972693306135a
HimanshuTiwariPlum <ht.28792@gmail.com>
2018-08-15 21:23:13 +0530

    Update the seed value as per the requirement.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["Adam", "bill", "tom", "cook"].each do |name|
+  User.create(name: name, email: "#{name}@gmail.com", number: "1234567890")
+end
+
+Product.create(name: 'One plus six', description: 'mobile', price: 39999)
+Product.create(name: 'One plus seven', description: 'mobile', price: 49999)
+Product.create(name: 'Iphone X', description: 'mobile', price: 99999)
